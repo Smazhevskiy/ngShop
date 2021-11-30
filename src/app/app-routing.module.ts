9236b331fd9fd27c6619e592b72934da6d54cde7
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-30 22:16:29 +0300

    admin menu routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,28 @@
-import {NgModule} from '@angular/core'
-import {RouterModule, Routes} from '@angular/router'
-import {MainLayoutComponent} from './shared/main-layout/main-layout.component'
-import {MainPageComponent} from './main-page/main-page.component'
-import {ProductPageComponent} from './product-page/product-page.component'
-import {CartPageComponent} from './cart-page/cart-page.component'
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import {AdminModule} from './admin/admin.module'
+
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
-      {path:'', redirectTo:'/', pathMatch:'full'},
+      {path: '', redirectTo: '/', pathMatch: 'full'},
       {path: '', component: MainPageComponent},
       {path: 'product/:id', component: ProductPageComponent},
-      {path: 'cart', component: CartPageComponent},
+      {path: 'cart', component: CartPageComponent}
     ]
+  },
+  {
+    path: 'admin', loadChildren: ()=> AdminModule  //'./admin/admin.module#AdminModule'   <=old syntax!!!
   }
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
