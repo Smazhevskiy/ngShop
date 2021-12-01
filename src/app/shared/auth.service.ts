ad9f899c1892135b79238c68716599c4a5e40e60
user_nitro_5 <timmy.lepel@gmail.com>
2021-12-01 16:34:16 +0300

    token + localStorage, logout

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
 import {environment} from '../../environments/environment'
+import {tap} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,37 @@ export class AuthService {
 
   login(User: any) {
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, User)
+      .pipe(
+        tap(this.setToken)
+      )
   }
+
+  private setToken(response: any) {
+    if (response) {
+      const expData = new Date(new Date().getTime() + +response.expiresIn * 1000)
+      localStorage.setItem('fb-token-exp', expData.toString())
+      localStorage.setItem('fb-token', response.idToken)
+    } else {
+      localStorage.clear()
+    }
+  }
+
+  get token() {
+    // @ts-ignore
+    let expDate = new Date(localStorage.getItem('fb-token-exp'))
+    if (new Date() > expDate) {
+      this.logout()
+      return null
+    }
+    return localStorage.getItem('fb-token')
+  }
+
+  logout() {
+    this.setToken(null)
+  }
+
+  IsAuthenticated() {
+    return !!this.token
+  }
+
 }
